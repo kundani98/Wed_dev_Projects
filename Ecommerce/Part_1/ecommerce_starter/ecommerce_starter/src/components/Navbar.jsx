1cc43bb6dc3417ebd5aff89fc3888e2cceab8f17
kundani98 <kcnemugumoni@gmail.com>
2023-07-28 09:37:18 +0200

    ecommerce

--- Ecommerce/Part_1/ecommerce_starter/ecommerce_starter/src/components/Navbar.jsx
+++ Ecommerce/Part_1/ecommerce_starter/ecommerce_starter/src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>KUNDANI98</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
